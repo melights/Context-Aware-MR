ffcf3d6826d64acce65647c572c42a835e9c2b2d
Karl Francis <i7241745@bournemouth.ac.uk>
2017-06-24 16:59:12 +0100

    refactored code so that material system just handles material interaction raycode moved into a new GameController class added some missing meta files

---
+++ UnityProject/Assets/Scripts/GameController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    [SerializeField]
+    private MaterialRayCastSystem m_materialRayCastSystem;
+
+    [SerializeField]
+    private Color m_errorCol;
+
+    [SerializeField]
+    private Camera m_renderCam;
+
+    [SerializeField]
+    private Text m_uiMaterialTextOutput;
+
+    [SerializeField]
+    private Image m_uiMaterialColourOutput;
+
+    [SerializeField]
+    private Transform m_debugHitPoint;
+
+    Vector3 lastHitPos = Vector3.zero;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray r = m_renderCam.ScreenPointToRay(Input.mousePosition);
+            APARaycastHit hit;
+            MaterialStruct mat;
+
+            if (m_materialRayCastSystem.RayVsSceneMaterial(r, out hit, out mat))
+            {
+                Color finalMaterialColour;
+                string finalMaterialName = "";
+                lastHitPos = hit.point;
+
+                if (mat != null)
+                {
+                    finalMaterialName = mat.m_name;
+                    finalMaterialColour = mat.m_colour;
+                }
+                else
+                {
+                    Debug.LogError("Couldn't Find material!");
+                    finalMaterialName = "Error";
+                    finalMaterialColour = m_errorCol;
+                }
+
+                m_uiMaterialColourOutput.color = finalMaterialColour;
+                m_uiMaterialTextOutput.text = finalMaterialName;
+            }
+            else
+            {
+                m_uiMaterialColourOutput.color = m_errorCol;
+                m_uiMaterialTextOutput.text = "No Data / Ray Missed ";
+            }
+        }
+
+        // Debug.DrawRay(r.origin, r.direction, Color.green, 100);
+
+        m_debugHitPoint.position = lastHitPos;
+    }
+}
--- UnityProject/Assets/Scripts/MaterialRayCastSystem.cs
+++ UnityProject/Assets/Scripts/MaterialRayCastSystem.cs
@@ -5,96 +5,17 @@ using UnityEngine.UI;
 
 public class MaterialRayCastSystem : MonoBehaviour {
 
-    // UI Elements to Update
-
-    [SerializeField]
-    private Text m_uiMaterialTextOutput;
-
-    [SerializeField]
-    private Image m_uiMaterialColourOutput;
-
     [SerializeField]
     private DataManager m_dataManager;
 
     [SerializeField]
-    private Transform m_debugHitPoint;
-
-    [SerializeField]
-    private Camera m_renderCam;
-
-    [SerializeField]
-    private Color m_errorCol;
-
-    [SerializeField]
     private GameObject m_materialMeshRenderer;
-
-    Vector3 lastHitPos = Vector3.zero;
 
     private MaterialStruct[] m_materialDataCopy;
 
     // Use this for initialization
     void Start () {
         m_materialDataCopy = m_dataManager.GetMaterialDataArray();
-    }
-	
-	// Update is called once per frame
-	void Update () {
- 
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray r = m_renderCam.ScreenPointToRay(Input.mousePosition);
-
-            APARaycastHit hit;
-            if (APARaycast.Raycast(r, out hit))
-            {
-                lastHitPos = hit.point;
-
-                Debug.Log("Hit " + hit.transform.gameObject.name);
-
-                var go = hit.transform.gameObject;
-                var goMat = go.GetComponent<Renderer>().material;
-                var matMainTex = goMat.mainTexture as Texture2D;
-                Vector2 pixelUv = hit.textureCoord;
-                pixelUv.x *= matMainTex.width;
-                pixelUv.y *= matMainTex.height;
-
-                var hitCol = matMainTex.GetPixel((int)pixelUv.x, (int)pixelUv.y);
-                
-                // find material struct
-                // todo: some sort of margin of error?
-                var mat = FindMaterialStructFromColour(hitCol, ref m_materialDataCopy);
-
-                string outputString = "";
-
-                if (mat != null)
-                {
-                    outputString = mat.m_name;
-                }
-                else
-                {
-                    Debug.LogError("Couldn't Find material!");
-                    outputString = "Error";
-                    hitCol = m_errorCol;
-                }
-
-                m_uiMaterialColourOutput.color = hitCol;
-                m_uiMaterialTextOutput.text = outputString;
-
-            }
-            else
-            {
-               // Debug.Log("Miss! " + APARaycast.intersectionErrorType);
-
-                m_uiMaterialColourOutput.color = m_errorCol;
-                m_uiMaterialTextOutput.text = "No Data / Ray Missed : " + APARaycast.intersectionErrorType;
-            }
-        }
-
-       // Debug.DrawRay(r.origin, r.direction, Color.green, 100);
-
-        m_debugHitPoint.position = lastHitPos;
-
     }
 
     private MaterialStruct FindMaterialStructFromColour(Color col, ref MaterialStruct[] ms)
@@ -115,9 +36,38 @@ public class MaterialRayCastSystem : MonoBehaviour {
         return null;
     }
 
+    public bool RayVsSceneMaterial(Ray r, out APARaycastHit hit, out MaterialStruct material)
+    {
+        hit = null;
+        material = null;
+
+        if (APARaycast.Raycast(r, out hit))
+        {
+            Debug.Log("Hit " + hit.transform.gameObject.name);
+
+            // Find The Material
+            var go = hit.transform.gameObject;
+            var goMat = go.GetComponent<Renderer>().material;
+            var matMainTex = goMat.mainTexture as Texture2D;
+            Vector2 pixelUv = hit.textureCoord;
+            pixelUv.x *= matMainTex.width;
+            pixelUv.y *= matMainTex.height;
+
+            var pixelColour = matMainTex.GetPixel((int)pixelUv.x, (int)pixelUv.y);
+            material = FindMaterialStructFromColour(pixelColour, ref m_materialDataCopy);
+
+            return true;
+        }
+        else
+        {
+            // Debug.Log("Miss! " + APARaycast.intersectionErrorType);
+
+            return false;
+        }
+    }
+
     public void ToggleMeshRenderer()
     {
         m_materialMeshRenderer.SetActive(!m_materialMeshRenderer.activeSelf);
     }
-
 }
