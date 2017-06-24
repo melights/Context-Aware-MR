b7754b0c40feabc90efbe0e386dfcb9abe2ec4bd
Karl Francis <i7241745@bournemouth.ac.uk>
2017-06-24 13:12:06 +0100

    added weapon struct and json data for game

--- UnityProject/Assets/Scripts/DataManager.cs
+++ UnityProject/Assets/Scripts/DataManager.cs
@@ -6,10 +6,12 @@ using SimpleJSON;
 public class DataManager : MonoBehaviour
 {
     private MaterialStruct[] m_materialData;
+    private WeaponStruct[] m_weaponData;
 
 	// Use this for initialization
 	void Start () {
         LoadMaterialData();
+        LoadWeaponData();
     }
 	
     private void LoadMaterialData()
@@ -30,14 +32,54 @@ public class DataManager : MonoBehaviour
             Color col = HexUtility.hexToColor(colHex);
             string hexColUnityFormat = HexUtility.colorToHex(col);
 
-            Debug.Log(name + hexColUnityFormat);
+            //Debug.Log(name + hexColUnityFormat);
 
             m_materialData[i] = new MaterialStruct(name, hexColUnityFormat, col);
         }
     }
 
+
+    private void LoadWeaponData()
+    {
+        var weaponDataParse = JSON.ParseFile("weapon_definitions");
+        var weaponDataArray = weaponDataParse["WEAPON_DEFINITIONS"].AsArray;
+        var weaponDataCount = weaponDataArray.Count;
+
+        m_weaponData = new WeaponStruct[weaponDataCount];
+
+        for (int i = 0; i < weaponDataCount; i++)
+        {
+            var thisWeapon = weaponDataArray[i];
+
+            string name = thisWeapon["NAME"];
+
+            string soundPathActivateWeapon = thisWeapon["SOUND_PATH_ACTIVATE_WEAPON"];
+
+            // note: the particle paths must have all material types defined in same order as 
+            // material definitions
+            List<string> particlePathHit = new List<string>();
+
+            for (int m = 0; m < m_materialData.Length; m++)
+            {
+                string matName = m_materialData[m].m_name.ToUpper();
+                string particleName = "PARTICLE_PATH_HIT_" + matName;
+                string path = thisWeapon[particleName];
+                particlePathHit.Add(path);
+            }
+
+            m_weaponData[i] = new WeaponStruct(name, soundPathActivateWeapon, particlePathHit);
+        }
+
+    }
+
+
     public MaterialStruct[] GetMaterialDataArray()
     {
         return m_materialData;
+    }
+
+    public WeaponStruct[] GetWeaponDataArray()
+    {
+        return m_weaponData;
     }
 }
---
+++ UnityProject/Assets/Scripts/WeaponStruct.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct WeaponStruct {
+
+    public string m_name;
+    public string m_soundPath;
+    public List<string> m_particleHitPaths;
+
+    public WeaponStruct(
+        string name,
+        string soundPath,
+        List<string> particleHitPaths
+
+        )
+    {
+        m_name = name;
+        m_soundPath = soundPath;
+        m_particleHitPaths = particleHitPaths;
+    }
+
+    //public string SOUND_PATH_ACTIVATE_WEAPON;
+
+    //public string PARTICLE_PATH_HIT_BRICK;
+    //public string PARTICLE_PATH_HIT_CARPET;
+    //public string PARTICLE_PATH_HIT_CERAMIC;
+    //public string PARTICLE_PATH_HIT_FABRIC;
+    //public string PARTICLE_PATH_HIT_FOLIAGE;
+    //public string PARTICLE_PATH_HIT_FOOD;
+    //public string PARTICLE_PATH_HIT_GLASS;
+    //public string PARTICLE_PATH_HIT_HAIR;
+    //public string PARTICLE_PATH_HIT_LEATHER;
+    //public string PARTICLE_PATH_HIT_METAL;
+    //public string PARTICLE_PATH_HIT_MIRROR;
+    //public string PARTICLE_PATH_HIT_OTHER;
+    //public string PARTICLE_PATH_HIT_PAINTED;
+    //public string PARTICLE_PATH_HIT_PAPER;
+    //public string PARTICLE_PATH_HIT_PLASTIC;
+    //public string PARTICLE_PATH_HIT_POLISHEDSTONE;
+    //public string PARTICLE_PATH_HIT_SKIN;
+    //public string PARTICLE_PATH_HIT_SKY;
+    //public string PARTICLE_PATH_HIT_STONE;
+    //public string PARTICLE_PATH_HIT_TILE;
+    //public string PARTICLE_PATH_HIT_WALLPAPER;
+    //public string PARTICLE_PATH_HIT_WATER;
+    //public string PARTICLE_PATH_HIT_WOOD;
+}
